af1530789032bb2fb627b5075fa636ddea20dbb7
Anggito02 <anggito_anju@yahoo.com>
2025-12-02 22:24:00 +0700

    [fix] enable core whitelist

--- src/main.ts
+++ src/main.ts
@@ -27,6 +27,8 @@ async function bootstrap() {
     app.enableCors({
         origin: true,
         credentials: true,
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
     });
 
     // Global serialization (untuk @Exclude, @Expose)
