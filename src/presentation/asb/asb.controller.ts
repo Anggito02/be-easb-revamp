4e340ccb90801b016af36da4e7730d900eacb0b8
Anggito02 <anggito_anju@yahoo.com>
2025-12-08 01:06:46 +0700

    [fix] asb controller endpoint

--- src/presentation/asb/asb.controller.ts
+++ src/presentation/asb/asb.controller.ts
@@ -83,7 +83,7 @@ export class AsbController {
     @Get('id')
     @Roles(Role.OPD, Role.VERIFIKATOR, Role.ADMIN, Role.SUPERADMIN)
     async findById(
-        @Query() id: number,
+        @Query('id', ParseIntPipe) id: number,
         @Req() req: Request,
     ): Promise<{ status: string; responseCode: number; message: string; data: AsbWithRelationsDto }> {
         const user = req.user as UserContext;
