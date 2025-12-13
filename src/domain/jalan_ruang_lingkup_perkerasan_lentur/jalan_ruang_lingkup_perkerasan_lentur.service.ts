ec57e54545c9b8c5ac12bb985a4b23c89a8b756f
Muhammad22064 <muhammad.panjerino@btn.co.id>
2025-12-14 03:15:24 +0700

    feat: add service implementation

--- src/domain/jalan_ruang_lingkup_perkerasan_lentur/jalan_ruang_lingkup_perkerasan_lentur.service.ts
+++ src/domain/jalan_ruang_lingkup_perkerasan_lentur/jalan_ruang_lingkup_perkerasan_lentur.service.ts
@@ -1,7 +1,7 @@
 import { CreateJalanRuangLingkupPerkerasanLenturDto } from "src/presentation/jalan_ruang_lingkup_perkerasan_lentur/dto/create_jalan_ruang_lingkup_perkerasan_lentur.dto";
 import { JalanRuangLingkupPerkerasanLentur } from "./jalan_ruang_lingkup_perkerasan_lentur.entity";
 import { UpdateJalanRuangLingkupPerkerasanLenturDto } from "src/presentation/jalan_ruang_lingkup_perkerasan_lentur/dto/update_jalan_ruang_lingkup_perkerasan_lentur.dto";
-import { GetJJalanRuangLingkupPerkerasanLenturDto } from "src/presentation/jalan_ruang_lingkup_perkerasan_lentur/dto/get_jalan_ruang_lingkup_perkerasan_lentur.dto";
+import { GetJalanRuangLingkupPerkerasanLenturDto } from "src/presentation/jalan_ruang_lingkup_perkerasan_lentur/dto/get_jalan_ruang_lingkup_perkerasan_lentur.dto";
 import { JalanRuangLingkupPerkerasanLenturPaginationResultDto } from "src/presentation/jalan_ruang_lingkup_perkerasan_lentur/dto/jalan_ruang_lingkup_perkerasan_lentur_pagination_result.dto";
 
 export abstract class JalanRuangLingkupPerkerasanLenturService {
@@ -9,6 +9,6 @@ export abstract class JalanRuangLingkupPerkerasanLenturService {
     abstract update(dto: UpdateJalanRuangLingkupPerkerasanLenturDto): Promise<JalanRuangLingkupPerkerasanLentur>;
     abstract delete(id: number): Promise<boolean>;
     abstract findById(id: number): Promise<JalanRuangLingkupPerkerasanLentur | null>;
-    abstract findAll(dto: GetJJalanRuangLingkupPerkerasanLenturDto): Promise<JalanRuangLingkupPerkerasanLenturPaginationResultDto>;
+    abstract findAll(dto: GetJalanRuangLingkupPerkerasanLenturDto): Promise<JalanRuangLingkupPerkerasanLenturPaginationResultDto>;
     abstract findByJenis(jenis: string): Promise<JalanRuangLingkupPerkerasanLentur | null>;
 }
